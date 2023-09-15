f617b7f81b814736e18929cfeca14faf22b51ebc
xXJimmyHackXx <jaime.jgvm@gmail.com>
2023-09-15 10:39:56 +0200

    ft_memmove || norminette

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -12,7 +12,7 @@
 
 #include "libre.h"
 
-void *ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char		*d;
 	unsigned const char	*s;
