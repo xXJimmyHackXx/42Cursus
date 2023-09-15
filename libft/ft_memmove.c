e10c95b7d9b49aa0a8a5208a1fb3e4d1c8236d4b
xXJimmyHackXx <jaime.jgvm@gmail.com>
2023-09-15 10:36:49 +0200

    ft_memmove

---
+++ libft/ft_memmove.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jagarci2 <jagarci2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/14 14:28:31 by jagarci2          #+#    #+#             */
+/*   Updated: 2023/09/14 14:28:31 by jagarci2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libre.h"
+
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	unsigned const char	*s;
+	size_t				i;
+
+	d = dest;
+	s = src;
+	i = 0;
+	if (d > s)
+	{
+		while (n > 0)
+		{
+			d[n - 1] = s[n - 1];
+			n--;
+		}
+	}
+	else
+	{
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	return (dest);
+}
--- libft/main.c
+++ libft/main.c
@@ -3,53 +3,78 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jagarci2 <jagarci2@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 16:18:52 by jagarci2          #+#    #+#             */
-/*   Updated: 2023/09/13 18:52:36 by jagarci2         ###   ########.fr       */
+/*   Updated: 2023/09/15 10:09:28 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libre.h"
 #include <stdio.h>
+#include <string.h>
 
 int	main(void)
 {
-	char	bzero[] = "hola, que tal";
+	char	fbzero[] = "hola, que tal";
+	char	obzero[] = "hola, que tal";
 	char	mset[] = "hola, que tal";
 	char	mstr2[] = "hola, que tal";
 	char	emstr2[100];
 	char	mcpy[] = "hola, que tal";
 	char	emcpy[100];
+	char	mm[] = "hola, que tal";
+	char	emm[100];
+	char	mm2[] = "hola, que tal";
+	char	emm2[100];
 
-	printf("\n");
+	printf("_______________\n");
 	printf("ISALNUM\n");
-	printf("FT: %d\n", ft_isalnum('1'));
-	printf("O: %d\n", isalnum('5'));
+	printf("FT: %d\n", ft_isalnum(' '));
+	printf("O: %d\n", isalnum(' '));
+	printf("_______________\n");
 	printf("ISALPHA\n");
-	printf("FT: %d\n", ft_isalpha('d'));
-	printf("O: %d\n", isalpha('d'));
+	printf("FT: %d\n", ft_isalpha(' '));
+	printf("O: %d\n", isalpha(' '));
+	printf("_______________\n");
 	printf("ISDIGIT\n");
-	printf("FT: %d\n", ft_isdigit('1'));
-	printf("O: %d\n", isdigit('1'));
+	printf("FT: %d\n", ft_isdigit(' '));
+	printf("O: %d\n", isdigit(' '));
+	printf("_______________\n");
 	printf("ISPRINT\n");
-	printf("FT: %d\n", ft_isprint('s'));
-	printf("O: %d\n", isprint('s'));
+	printf("FT: %d\n", ft_isprint(' '));
+	printf("O: %d\n", isprint(' '));
+	printf("_______________\n");
 	printf("ISASCII\n");
-	printf("FT: %d\n", ft_isascii('3'));
-	printf("O: %d\n", isascii('3'));
+	printf("FT: %d\n", ft_isascii(' '));
+	printf("O: %d\n", isascii(' '));
+	printf("_______________\n");
 	printf("STRLEN\n");
 	printf("FT: %lu\n", ft_strlen("balablab"));
 	printf("O: %lu\n", strlen("balablab"));
+	printf("_______________\n");
 	printf("MEMSET\n");
-	printf("FT: %s\n", ft_memset(mset, '$', 5));
-	printf("O: %s\n", memset(mset, '$', 5));
+	ft_memset(mset, '$', 5);
+	printf("FT: %s\n", mset);
+	memset(mset, '$', 5);
+	printf("O: %s\n", mset);
+	printf("_______________\n");
 	printf("MEMCPY\n");
 	ft_memcpy(emcpy, mcpy, sizeof(mcpy));
 	printf("FT: %s\n", emcpy);
-	printf("O: %s\n", memcpy(emstr2, mstr2, sizeof(mstr2)));
+	memcpy(emstr2, mstr2, sizeof(mstr2));
+    printf("O: %s\n", emstr2);
+	printf("_______________\n");
 	printf("BZERO\n");
-	ft_bzero(bzero, 2);
-	printf("FT: %s\n", bzero);
-	printf("O: %s\n", bzero(bzero, 2));
+	ft_bzero(fbzero, 2);
+	printf("FT: %s\n", fbzero);
+	bzero(obzero, 2);
+	printf("O: %s\n", obzero);
+	printf("_______________\n");
+	printf("MEMMOVE\n");
+	ft_memmove(emm2, mm2, sizeof(mm2));
+	printf("FT: %s\n", emm2);
+	memmove(emm, mm, sizeof(mm));
+	printf("O: %s\n", emm);
+	
 }
