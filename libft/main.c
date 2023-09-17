0a58aa7992e654d2666215f824b34251f729159d
xXJimmyHackXx <jaime.jgvm@gmail.com>
2023-09-17 21:58:54 +0200

    funcional en 42

--- libft/main.c
+++ libft/main.c
@@ -3,17 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jagarci2 <jagarci2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 16:18:52 by jagarci2          #+#    #+#             */
-/*   Updated: 2023/09/15 14:06:08 by marvin           ###   ########.fr       */
+/*   Updated: 2023/09/17 21:58:14 by jagarci2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libre.h"
 #include <stdio.h>
 #include <string.h>
-#include <bsd/string.h>
 
 int	main(void)
 {
@@ -33,60 +32,59 @@ int	main(void)
 	char	strl2[] = "hola, que";
 	char	estrl2[100];
 
-	printf("_______________\n");
+	printf("----------------\n");
 	printf("ISALNUM\n");
 	printf("FT: %d\n", ft_isalnum(' '));
 	printf("O: %d\n", isalnum(' '));
-	printf("_______________\n");
+	printf("----------------\n");
 	printf("ISALPHA\n");
 	printf("FT: %d\n", ft_isalpha(' '));
 	printf("O: %d\n", isalpha(' '));
-	printf("_______________\n");
+	printf("----------------\n");
 	printf("ISDIGIT\n");
 	printf("FT: %d\n", ft_isdigit(' '));
 	printf("O: %d\n", isdigit(' '));
-	printf("_______________\n");
+	printf("----------------\n");
 	printf("ISPRINT\n");
 	printf("FT: %d\n", ft_isprint(' '));
 	printf("O: %d\n", isprint(' '));
-	printf("_______________\n");
+	printf("----------------\n");
 	printf("ISASCII\n");
 	printf("FT: %d\n", ft_isascii(' '));
 	printf("O: %d\n", isascii(' '));
-	printf("_______________\n");
+	printf("----------------\n");
 	printf("STRLEN\n");
 	printf("FT: %lu\n", ft_strlen("balablab"));
 	printf("O: %lu\n", strlen("balablab"));
-	printf("_______________\n");
+	printf("----------------\n");
 	printf("MEMSET\n");
 	ft_memset(mset, '$', 5);
 	printf("FT: %s\n", mset);
 	memset(mset, '$', 5);
 	printf("O: %s\n", mset);
-	printf("_______________\n");
+	printf("----------------\n");
 	printf("MEMCPY\n");
 	ft_memcpy(emcpy, mcpy, sizeof(mcpy));
 	printf("FT: %s\n", emcpy);
 	memcpy(emstr2, mstr2, sizeof(mstr2));
-    printf("O: %s\n", emstr2);
-	printf("_______________\n");
+	printf("O: %s\n", emstr2);
+	printf("----------------\n");
 	printf("BZERO\n");
 	ft_bzero(fbzero, 2);
 	printf("FT: %s\n", fbzero);
 	bzero(obzero, 2);
 	printf("O: %s\n", obzero);
-	printf("_______________\n");
+	printf("----------------\n");
 	printf("MEMMOVE\n");
 	ft_memmove(emm2, mm2, sizeof(mm2));
 	printf("FT: %s\n", emm2);
 	memmove(emm, mm, sizeof(mm));
 	printf("O: %s\n", emm);
-	printf("_______________\n");
+	printf("----------------\n");
 	printf("STRLCPY\n");
 	ft_strlcpy(estrl2, strl2, sizeof(strl2));
 	printf("FT: %s || LEN: %ld\n", estrl2, sizeof(strl2));
-	strlcpy(estl, strl, sizeof(strl));
+	strlcpy(estl, strl, sizeof(estl));
 	printf("O: %s || LEN: %ld\n", estl, sizeof(strl));
-	printf("_______________\n");
-	
+	printf("----------------\n");
 }
