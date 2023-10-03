dbc7740fc3d614e5d26af940f5adfe9cf4e68066
xXJimmyHackXx <jaime.jgvm@gmail.com>
2023-10-03 21:46:44 +0200

    cena

--- libft/ft_lstsize.c
+++ libft/ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: jagarci2 <jagarci2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 16:08:15 by jagarci2          #+#    #+#             */
-/*   Updated: 2023/10/03 16:53:55 by jagarci2         ###   ########.fr       */
+/*   Updated: 2023/10/03 20:48:10 by jagarci2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,24 @@ int	ft_lstsize(t_list *lst)
 
 	i = 0;
 	tmp = lst;
-	while (tmp->next != NULL)
+	while (tmp && tmp->next)
 	{
 		i++;
 		tmp = tmp->next;
 	}
-	if (tmp->next == NULL)
+	if (tmp && tmp->next == NULL)
 		i++;
 	return (i);
 }
+// int	main()
+// {
+// 	char str[8] = "holaasd";
+// 	t_list *lst = ft_lstnew(str);
+// 	for (int i = 0;  i < 20; i++)
+// 	{
+// 		ft_lstadd_front(&lst, ft_lstnew(str));
+// 	}
+// 	lst->next->next->next = NULL;
+// 	lst = NULL;
+// 	printf("lst_count ->%d\n", ft_lstsize(lst));
+// }
