23f4bb651469032d05276df4063e2e46f91fb13b
xXJimmyHackXx <jaime.jgvm@gmail.com>
2023-11-22 13:36:46 +0100

    libft 125%

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -3,48 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jimmy <jimmy@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jagarci2 <jagarci2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 13:46:30 by kali              #+#    #+#             */
-/*   Updated: 2023/10/31 12:49:52 by jimmy            ###   ########.fr       */
+/*   Updated: 2023/11/22 13:34:22 by jagarci2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_lstclear_miau(t_list **lst, void (*del)(void *))
-{
-	t_list	*next;
-
-	while (*lst)
-	{
-		next = (*lst)->next;
-		del((*lst)->content);
-		free(*lst);
-		*lst = next;
-	}
-}
-
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new;
-	t_list	*val;
-	void	*content;
+	t_list	*l;
+	t_list	*fok;
+	void	*a;
 
-	if (!lst || !f || !del)
-		return (0);
-	new = NULL;
-	while (lst)
+	l = 0;
+	while (lst != NULL && (*f) && (*del))
 	{
-		content = f(lst->content);
-		val = ft_lstnew(content);
-		if (!val)
+		a = (*f)(lst->content);
+		fok = ft_lstnew(a);
+		if (!fok || !a)
 		{
-			ft_lstclear_miau(&new, del);
+			del(a);
+			ft_lstclear(&l, (*del));
+			free(fok);
 			return (0);
 		}
-		ft_lstadd_back(&new, val);
+		ft_lstadd_back(&l, fok);
 		lst = lst->next;
 	}
-	return (new);
+	return (l);
 }
