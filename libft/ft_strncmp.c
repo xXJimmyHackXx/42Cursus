67436b5502bde5a89db72aed0f6ef6d1c61faee1
xXJimmyHackXx <jaime.jgvm@gmail.com>
2023-11-22 20:01:28 +0100

    125%

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jagarci2 <jagarci2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 20:56:37 by jagarci2          #+#    #+#             */
-/*   Updated: 2023/09/23 12:40:11 by jagarci2         ###   ########.fr       */
+/*   Updated: 2023/11/22 20:00:59 by jagarci2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	if (n > 0)
 	{
 		while (str1[i] != '\0' && str2[i] != '\0' && i < (n - 1))
-		{	
+		{
 			if (str1[i] != str2[i])
 				return (str1[i] - str2[i]);
 			i++;
