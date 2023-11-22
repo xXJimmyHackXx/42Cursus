72def6b2152bdccde7938ffa2befe95eadc70538
xXJimmyHackXx <jaime.jgvm@gmail.com>
2023-11-22 14:52:24 +0100

    gnl

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jagarci2 <jagarci2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/25 17:12:36 by jimmy             #+#    #+#             */
+/*   Updated: 2023/11/21 19:57:18 by jagarci2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+// // void leaks()
+// // {
+// // 	system("leaks -q a.out");
+// // }
+// int	main(void)
+// {
+// 	// atexit(leaks);
+// 	int fd = open("fd1.txt", O_RDONLY);
+// 	char *line;
+
+// 	if (fd == -1)
+// 	{
+// 		printf("Error al abrir el archivo");
+// 		return (1);
+// 	}
+// 	while ((line = get_next_line(fd)) != NULL)
+// 	{
+// 		printf("%s", line);
+// 		free(line);
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
+
+char	*ft_readed_line(char *start)
+{
+	int		i;
+	char	*line;
+
+	if (!start || !start[0])
+		return (NULL);
+	i = 0;
+	while (start[i] && start[i] != '\n')
+		i++;
+	if (start[i] == '\n')
+		i++;
+	line = (char *)malloc(1 + i * sizeof(char));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (start[i] && start[i] != '\n')
+	{
+		line[i] = start[i];
+		i++;
+	}
+	if (start[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+char	*ft_move_start(char	*start)
+{
+	char	*new_buff;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (start[i] && start[i] != '\n')
+		i++;
+	if (start[i] == '\0')
+	{
+		free(start);
+		return (NULL);
+	}
+	i += (start[i] == '\n');
+	new_buff = (char *)malloc(1 + ft_strlen(start) - i);
+	if (!new_buff)
+		return (NULL);
+	j = 0;
+	while (start[i + j])
+	{
+		new_buff[j] = start[i + j];
+		j++;
+	}
+	new_buff[j] = '\0';
+	free(start);
+	return (new_buff);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*tmp;
+	int			fd_read;
+	static char	*start_str;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	fd_read = 1;
+	tmp = (char *)malloc(1 + BUFFER_SIZE * sizeof(char));
+	if (!tmp)
+		return (NULL);
+	while (!(ft_strchr(start_str, '\n')) && fd_read != 0)
+	{
+		fd_read = read(fd, tmp, BUFFER_SIZE);
+		if (fd_read == -1)
+		{
+			free(tmp);
+			if (start_str)
+				free(start_str);
+			return (start_str = NULL, NULL);
+		}
+		tmp[fd_read] = '\0';
+		start_str = ft_strjoin(start_str, tmp);
+		if (!start_str)
+			return (ft_free(start_str, NULL));
+	}
+	free(tmp);
+	tmp = ft_readed_line(start_str);
+	start_str = ft_move_start(start_str);
+	return (tmp);
+}
