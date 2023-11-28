641baf61f989f6ae62537cea8edd2daa99deac7a
xXJimmyHackXx <jaime.jgvm@gmail.com>
2023-11-28 19:45:02 +0100

    poca cosa

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,16 +6,29 @@
 /*   By: jagarci2 <jagarci2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 19:31:51 by jagarci2          #+#    #+#             */
-/*   Updated: 2023/10/20 16:56:44 by jagarci2         ###   ########.fr       */
+/*   Updated: 2023/11/28 19:33:16 by jagarci2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 
+ft_var_type()
+{
+	
+}
+
 int	ft_printf(char const *str, ...)
 {
+	int		i;
 	va_list	ptr;
 
 	va_start (ptr, str);
-	
+	while (str)
+	{
+		if (str[i] == "%")
+		{
+			i++;
+			ft_var_type()
+		}
+	}
 }
